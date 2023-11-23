488c2496e682ee0dadefd081df152b6a6a7193ce
erofeev denis <deniserofeev84@gmail>
2023-11-23 16:58:24 +0300

    create an object through which we will communicate with our bot create an object through which we will communicate with our bot

--- tgbot-teller.go
+++ tgbot-teller.go
@@ -1,5 +1,10 @@
 package main
 
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+// create an object through which we will communicate with our bot
+var bot *tgbotapi.BotAPI
+
 func main() {
 	//TO DO
 

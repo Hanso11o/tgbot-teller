2d499fef35fb61b6ff627869e3b8bbff5c16b8cb
erofeev denis <deniserofeev84@gmail>
2023-11-24 13:23:13 +0300

    create func connectWithTelegram()

--- tgbot-teller.go
+++ tgbot-teller.go
@@ -1,11 +1,26 @@
 package main
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 // create an object through which we will communicate with our bot
 var bot *tgbotapi.BotAPI
 
+func connectWithTelegram() {
+	var err error
+	var token string
+	bot, err = tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Fatal("cannot connect to telegram")
+		_ = token
+
+	}
+}
+
 func main() {
-	//TO DO
+	connectWithTelegram()
 
 }

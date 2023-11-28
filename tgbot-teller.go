3fe8e57559a76284c1fecf0fbe1ac5b4dab268ce
erofeev denis <deniserofeev84@gmail>
2023-11-28 15:23:17 +0300

    add func sendMessage and send Answer

--- tgbot-teller.go
+++ tgbot-teller.go
@@ -3,13 +3,18 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 // create an object through which we will communicate with our bot
 var bot *tgbotapi.BotAPI
+// create name's bot
+var tellerBotName = [2]string{"teller", "tell me"}
 
+
+// creating a function to connect to a telegram
 func connectWithTelegram() {
 	var err error
 	var token string
@@ -21,12 +26,39 @@ func connectWithTelegram() {
 	}
 }
 
+func sendMessage(msg string) {
+	// TODO
+}
+
+func isMessageToBot(update *tgbotapi.Update) bool {
+	if update.Message == nil || update.Message.Text  == "" {
+		return false
+
+	}
+
+	msgToLower := strings.ToLower(update.Message.Text)
+	for _, name := range tellerBotName {
+		if strings.Contains(msgToLower, name ) {
+			return true
+		}
+	}
+	return false
+
+}
+
+func sendAnswer(update *tgbotapi.Update)
+
 func main() {
 	connectWithTelegram()
 
 	updateConfig := tgbotapi.NewUpdate(0)
 	for update := range bot.GetUpdatesChan(updateConfig) {
-		if update.Message != nil && update.Message.Text =="/start" 
+		if update.Message != nil && update.Message.Text == "/start" {
+			sendMessage("Задай вопрос, назвав меня по имени teller или tell me. Ответом на вопрос должны быть \"да\" либо \"Нет\"")
+		}
+		if isMessageToBot(&update)
+		sendAnswer(&update)
+
 	}
 
 }
